f37ca82f61f034d5d9ba4f9e26fcce64b0ea8550
Ethan Stone <ethanstone@berkeley.edu>
2025-12-01 01:03:51 -0800

    Orthogonalize Initialization of Linear Layer Weight Matrices

--- scripts/training.py
+++ scripts/training.py
@@ -9,6 +9,7 @@ from ..optimizers.muonW1 import MuonW
 from ..optimizers.manifold_muonW import ManifoldMuonW
 from ..config_types.config_types import OptimizerKwargs, ExperimentConfig
 from ..model.model import make_model
+from ..model.model import orthogonal_init
 from ..scripts.dataset import get_batch as get_ols_batch
 import datetime
 
@@ -325,6 +326,9 @@ def run_from_config(config: ExperimentConfig):
             opt_kwargs[k] = optimizer_kwargs[k]
 
     optimizer = optimizer_class(model.parameters(), **opt_kwargs)
+
+    if resume_from is None:
+        model.apply(orthogonal_init)
 
     scheduler = WarmupConstantDecayLrScheduler(optimizer, num_steps)
     model = train(
