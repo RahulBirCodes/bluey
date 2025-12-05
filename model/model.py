fixed mha scaling and loss

model/model.py
@@ -105,7 +105,9 @@ class MultiHeadAttention(nn.Module):
     B, H, T, D = Q.shape
     Q, K = self.rope(Q, K)
     attn_scores = torch.matmul(Q, K.transpose(-2, -1))
-    attn_scores = attn_scores / (self.d_k if self.lips else self.d_k ** 0.5)
+    # We do not scale by d_k for lips because our data generation independently samples the inputs
+    # attn_scores = attn_scores / (self.d_k if self.lips else self.d_k ** 0.5)
+    attn_scores = attn_scores / self.d_k ** 0.5
     mask = torch.tril(torch.ones(T, T, device=Q.device))
     attn_scores= attn_scores.masked_fill(mask == 0, -float("inf"))
     attn_probs = F.softmax(attn_scores, dim=-1)

scripts/training.py
@@ -219,7 +219,7 @@ def train(
         B, S, D = outputs.shape
         b_idx = torch.arange(B, device=device).unsqueeze(1)
         y_pred = outputs[b_idx, x_token_indices, :]
-        loss = torch.sum((y_pred-Y)**2, dim=1).mean()
+        loss = torch.sum((y_pred-Y)**2, dim=2).mean()
 
         optimizer.zero_grad()
         loss.backward()