90cfc7cdb9edd966da8a84212e069a99d3f5a223
Rahul Bir <rahulbircodez@gmail.com>
2025-12-06 13:29:50 -0800

    added gradient clipping and log

--- scripts/training.py
+++ scripts/training.py
@@ -223,6 +223,8 @@ def train(
 
         optimizer.zero_grad()
         loss.backward()
+        if isinstance(optimizer, ManifoldMuon):
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer_step_fn(optimizer)
         if scheduler is not None:
             scheduler.step()
